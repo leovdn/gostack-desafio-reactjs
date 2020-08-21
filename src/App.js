d0b6adfa21a3e3cd72df1d01c461cb70cef3d2c2
leovdn <leo_vdn@hotmail.com>
2020-08-21 13:24:04 -0300

    finished ReactJS challenge; created integration with api to list, add and remove items from the backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 import "./styles.css";
 
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+      console.log(response.data);
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: "Mobile com React Native",
+      url: "https://github.com/leovdn/",
+      techs: "React Native, NodeJS"
+    });
+
+    const repo = response.data;
+
+    setRepositories([...repositories, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    
+    const noDeleted = repositories.filter((repository) => repository.id !== id);
+
+    setRepositories(noDeleted);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => {
+          return <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
